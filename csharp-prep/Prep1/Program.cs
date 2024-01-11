5f33eec745dfc4b471a48caacac7301f779b3bce
justinsullivan2004 <sul22007@byui.edu>
2024-01-11 10:19:52 -0700

    commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("Whats your age?");
+
+        var ageString = Console.ReadLine();
+        System.Console.WriteLine($"My age is {ageString}.");
+        System.Console.WriteLine("My age is " + ageString + ".");
+
+        int age = int.Parse(ageString);
+
+        if (age < 18) 
+        {
+            System.Console.WriteLine("You are a minor.");
+        }
+        else 
+        {
+            System.Console.WriteLine("you are old");
+        }
+
     }
 }
