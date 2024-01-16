1b495ed1a38a832138af104c559deead4d0cbc2b
justinsullivan2004 <sul22007@byui.edu>
2024-01-16 12:32:45 -0700

    commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        int guess_number = 0;
+
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+
+        while (guess_number != magic_number);
+        {
+            System.Console.WriteLine("What is your guess? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (guess_number > magic_number)
+            {
+                System.Console.WriteLine("Lower");
+            }
+            if (guess_number < magic_number)
+            {
+                System.Console.WriteLine("Higher");
+            }
+            else
+            {
+                System.Console.WriteLine("Congrats! You guessed it. ");
+            }
+
+        } 
+
+
+
+
+    
+
     }
 }
